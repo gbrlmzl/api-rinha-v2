b416d6b94a9c462f635ecfa0aa768146852f229c
Gabriel Mizael <gabrielmzl100@gmail.com>
2026-05-12 13:06:24 -0300

    Exclusão de classes inutilizadas

--- src/main/java/rinhacampusiv/api/v2/validators/tournamentRetryRegister/TournamentRetryRegisterValidator.java
+++
@@ -1,9 +0,0 @@
-package rinhacampusiv.api.v2.validators.tournamentRetryRegister;
-
-
-import rinhacampusiv.api.v2.domain.tournaments.teams.Team;
-import rinhacampusiv.api.v2.domain.tournaments.tournaments.Tournament;
-
-public interface TournamentRetryRegisterValidator {
-    void validate(Tournament tournament, Team team );
-}
